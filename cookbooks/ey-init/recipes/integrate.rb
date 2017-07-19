edac46468b30448c94a561bebfe9b6218ab5e59d
John Dalton <john@johndalton.info>
2017-07-19 17:12:53 +1000

    Run custom chef during quick/integration runs

--- cookbooks/ey-init/recipes/integrate.rb
+++ cookbooks/ey-init/recipes/integrate.rb
@@ -6,7 +6,11 @@ when 'app', 'app_master'
 when 'util'
 when /^db/
   if node.engineyard.environment['db_stack_name'][/^postgres/]
+    # Adding/removing an instance may change where the backup is run
     include_recipe 'ey-backup::postgres'
   end
 end
 include_recipe "ssh_keys" # CC-691 - update ssh whitelist after takeovers
+
+# Custom recipes must be run when instances are added/removed.
+include_recipe 'ey-custom::after-main'
